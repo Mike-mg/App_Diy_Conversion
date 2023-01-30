059425a3564b30ee8b1a14148a8ef25cf0fb80ae
Michael.Mg <michael.mg.09a@gmail.com>
2023-01-30 19:19:14 +0100

    update mod_frames

--- pkg_widgets/mod_frames.py
+++ pkg_widgets/mod_frames.py
@@ -10,7 +10,8 @@ class FramesWidgets:
     def __init__(self) -> None:
 
         self.bg = "#f28de2"
-        self.dict_percent_dosage_aroma = {"aroma": self.selected_percent_aroma.get()}, {"base": 100}
+        self.dict_percent_dosage_aroma = {
+            "aroma": self.selected_percent_aroma.get()}, {"base": 100}
 
 # Command by quantity total ---------------------------------------------------
 
@@ -18,13 +19,22 @@ class FramesWidgets:
 
         try:
 
-            if self.entry_value_entry_quantity_total.get() > 0:
+            if self.value_entry_quantity_total.get() > 0:
 
-                base = self.entry_value_entry_quantity_total.get() * self.dict_percent_dosage_aroma[1]["base"] / (self.dict_percent_dosage_aroma[0]["aroma"] + self.dict_percent_dosage_aroma[1]["base"])
+                base = self.value_entry_quantity_total.get() \
+                    * self.dict_percent_dosage_aroma[1]["base"] \
+                    / (self.dict_percent_dosage_aroma[0]["aroma"]
+                       + self.dict_percent_dosage_aroma[1]["base"])
+
                 base = round(base, 1)
-                aroma = base * self.selected_percent_aroma[0]["aroma"] / self.dict_percent_dosage_aroma[1]["base"]
+                aroma = base * self.selected_percent_aroma[0]["aroma"] \
+                    / self.dict_percent_dosage_aroma[1]["base"]
+
                 aroma = round(aroma, 1)
-                self.value_show_quantity_total.set(self.entry_value_entry_quantity_total.get())
+
+                self.value_show_quantity_total.set(
+                    self.value_entry_quantity_total.get())
+
                 self.value_show_aroma.set(aroma)
                 self.value_show_base.set(base)
 
@@ -32,7 +42,7 @@ class FramesWidgets:
             messagebox.showerror(
                 title="Erreur de saisie",
                 message="Erreur de saisie\nTous les champs non utilisés \
-                    doivent être à 0.\nLes champs vont être réinitialisés") # noqa
+                doivent être à 0.\nLes champs vont être réinitialisés") # noqa
 
 # Command by aroma ------------------------------------------------------------
 
@@ -41,15 +51,22 @@ class FramesWidgets:
         try:
             if self.entry_aroma.get() > 0:
 
-                base = self.entry_aroma.get() * self.dict_percent_dosage_aroma[1]["base"] / self.dict_percent_dosage_aroma[0]["aroma"]
+                base = self.entry_aroma.get() \
+                    * self.dict_percent_dosage_aroma[1]["base"] \
+                    / self.dict_percent_dosage_aroma[0]["aroma"]
+
                 base = round(base, 1)
-                self.value_show_quantity_total.set(base + self.entry_aroma.get())
+
+                self.value_show_quantity_total.set(
+                    base + self.entry_aroma.get())
+
                 self.value_show_base.set(base)
                 self.value_show_aroma.set(self.entry_aroma.get)
 
         except Exception:
-            messagebox.showerror(title="Erreur de saisie",
-            message="Erreur de saisie\nTous les champs non utilisés \
+            messagebox.showerror(
+                title="Erreur de saisie",
+                message="Erreur de saisie\nTous les champs non utilisés \
                 doivent être à 0.\nLes champs vont être réinitialisés") # noqa
 
 # Command by base -------------------------------------------------------------
@@ -60,16 +77,23 @@ class FramesWidgets:
 
             if self.entry_base.get() > 0:
 
-                aroma = self.dict_percent_dosage_aroma[0]["aroma"] * self.dict_percent_dosage_aroma[0]["aroma"] / self.dict_percent_dosage_aroma[0]["aroma"]
-                aroma = round(aroma, 2)
-                self.value_show_quantity_total.set(self.entry_base.get() + aroma)
+                aroma = self.dict_percent_dosage_aroma[0]["aroma"] \
+                    * self.entry_base.get() \
+                    / self.dict_percent_dosage_aroma[1]["base"]
+
+                aroma = round(aroma, 1)
+
+                self.value_show_quantity_total.set(
+                    self.entry_base.get() + aroma)
+
                 self.value_show_aroma.set(aroma)
                 self.value_show_base.set(self.entry_base.get())
 
         except Exception:
             messagebox.showerror(
                 title="Erreur de saisie",
-                message="Erreur de saisie\nTous les champs non utilisés doivent être à 0.\nLes champs vont être réinitialisés") # noqa
+                message="Erreur de saisie\nTous les champs non utilisés \
+                doivent être à 0.\nLes champs vont être réinitialisés") # noqa
 
 # Frame percent aroma selected ------------------------------------------------
 
@@ -129,10 +153,10 @@ class FramesWidgets:
         self.show_entry_quantity_total.grid(
             row=1, column=0, padx=(0, 0), pady=(0, 15))
 
-        self.entry_value_entry_quantity_total = tk.DoubleVar()
+        self.value_entry_quantity_total = tk.DoubleVar()
         self.entry_entry_quantity_total = tk.Entry(
             frame_quantity_total,
-            textvariable=self.entry_value_entry_quantity_total)
+            textvariable=self.value_entry_quantity_total)
 
         self.entry_entry_quantity_total.config(
             width=10, justify="center", bg="white", relief="ridge", bd=2)
@@ -215,7 +239,8 @@ class FramesWidgets:
         self.show_base.grid(row=1, column=0, padx=(0, 0), pady=(0, 15))
 
         self.entry_value_base = tk.DoubleVar()
-        self.entry_base = tk.Entry(frame_base, textvariable=self.entry_value_base)
+        self.entry_base = tk.Entry(
+            frame_base, textvariable=self.entry_value_base)
         self.entry_base.config(
             width=10, justify="center", bg="white", relief="ridge", bd=2)
 
