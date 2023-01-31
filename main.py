352acc6083a620b41125809a7bfc1f6fb3bc9319
Michael.Mg <michael.mg.09a@gmail.com>
2023-01-31 11:24:30 +0100

    update main and pkg_window_tkinter

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ class Main:
 
 start_window = Main()
 
-start_window.main_window.verify_sysytem_for_icon()
+start_window.main_window.verify_system_for_icon()
 start_window.main_window.config_window()
+start_window.main_window.background_window()
 
 start_window.main_window.show_frame_percent_aroma_selected()
 start_window.main_window.show_show_frame_quantity_total()
 start_window.main_window.show_frame_aroma()
 start_window.main_window.show_frame_base()
 
-start_window.main_window.window.mainloop()
+start_window.main_window.mainloop()
--- pkg_window_tkinter/mod_window_main.py
+++ pkg_window_tkinter/mod_window_main.py
@@ -6,52 +6,56 @@ from PIL import Image, ImageTk
 import pkg_widgets
 
 
-class MainWindowApp:
+class MainWindowApp(tk.Tk):
     # Main window
 
     def __init__(self):
+        super().__init__()
 
-        self.window = tk.Tk()
         self.bg = "#f28de2"
         self.background = ImageTk.PhotoImage(
             Image.open("images/background.png"))
 
         self.widgets = pkg_widgets.FramesWidgets()
 
-    def verify_sysytem_for_icon(self) -> None:
+    def verify_system_for_icon(self) -> None:
         # Displays the icon according to the system
 
         if sys.platform.startswith("linux"):
             icon = tk.PhotoImage(file="images/icone.gif")
-            self.window.call("wm", "iconphoto", self.window._w, icon)
+            self.call("wm", "iconphoto", self._w, icon)
 
         elif sys.platform.startswith("win32"):
-            self.window.iconbitmap("images/icone.ico")
+            self.iconbitmap("images/icone.ico")
 
     def config_window(self) -> None:
         # Configure the main window
 
-        self.window.title("Diy - Vap")
-        self.window.geometry("522x575")
-        # self.window.resizable(0, 0)
-        tk.Label(self.window, image=self.background).place(x=1, y=0)
+        self.title("Diy - Vap")
+        self.geometry("522x575")
+        self.resizable(0, 0)
 
-    def show_frame_percent_aroma_selected(self):
+    def background_window(self) -> None:
+        # Background window
+
+        tk.Label(self, image=self.background).place(x=1, y=0)
+
+    def show_frame_percent_aroma_selected(self) -> None:
         # Show frame select aroma percent
 
-        self.widgets.frame_percent_aroma_selected(self.window)
+        self.widgets.frame_percent_aroma_selected(self)
 
-    def show_show_frame_quantity_total(self):
+    def show_show_frame_quantity_total(self) -> None:
         # Show frame quantity
 
-        self.widgets.frame_quantity_total(self.window)
+        self.widgets.frame_quantity_total(self)
 
-    def show_frame_aroma(self):
+    def show_frame_aroma(self) -> None:
         # Show frame aroma
 
-        self.widgets.frame_aroma(self.window)
+        self.widgets.frame_aroma(self)
 
-    def show_frame_base(self):
+    def show_frame_base(self) -> None:
         # Show frame base
 
-        self.widgets.frame_base(self.window)
+        self.widgets.frame_base(self)
